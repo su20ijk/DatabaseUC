1cdf51a9a205f55a32ca63282eb583570d05c9ba
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-03-07 20:16:26 -0600

    Added all Pounds to Kilos

---
+++ DatabaseActivities/Controllers/PoundToKiloController.cs
@@ -0,0 +1,53 @@
+﻿using DatabaseActivities.Models.Entity;
+using DatabaseActivities.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseActivities.Controllers
+{
+    public class PoundToKiloController : Controller
+    {
+        private PoundToKiloService service = new PoundToKiloService();
+        // GET: PoundToKilo
+        public ActionResult Index()
+        {
+            return View(service.GetAllConversions());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateConversion(double Pounds)
+        {
+            service.AddConversion(Pounds);
+            return RedirectToAction("Index", "PoundToKilo");
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EditConversion(PoundToKilo toSave)
+        {
+            service.SaveEdits(toSave);
+            return RedirectToAction("Index", "PoundToKilo");
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DeleteConversion(int Id)
+        {
+            service.DeleteConversion(Id);
+            return RedirectToAction("Index", "PoundToKilo");
+        }
+    }
+}
---
+++ DatabaseActivities/Service/PoundToKiloService.cs
@@ -0,0 +1,43 @@
+﻿using DatabaseActivities.Models.Entity;
+using DatabaseActivities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseActivities.Service
+{
+    public class PoundToKiloService
+    {
+        private PoundToKiloRepository repository;
+
+        public PoundToKiloService()
+        {
+            repository = new PoundToKiloRepository();
+        }
+
+        public List<PoundToKilo> GetAllConversions()
+        {
+            return repository.GetAllConversions();
+        }
+
+        public void AddConversion(double Pounds)
+        {
+            double Kilograms = Pounds * 0.453592;
+            PoundToKilo temp = new PoundToKilo();
+            temp.Pounds = Pounds;
+            temp.Kilograms = Kilograms;
+            repository.AddUnitConversion(temp);
+        }
+
+        public void DeleteConversion(int Id)
+        {
+            repository.DeleteUnitConversion(Id);
+        }
+
+        public void SaveEdits(PoundToKilo toSave)
+        {
+            repository.SaveEdits(toSave);
+        }
+    }
+}
