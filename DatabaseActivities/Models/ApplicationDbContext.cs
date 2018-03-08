1dc6be168abb6534134b72df89dfacb63706a27f
Unknown <19syverj@elmbrookstudents.org>
2018-03-07 19:27:18 -0600

    Rebuilt

--- DatabaseActivities/Models/ApplicationDbContext.cs
+++ DatabaseActivities/Models/ApplicationDbContext.cs
@@ -26,6 +26,6 @@ namespace DatabaseActivities.Models
         }
         public DbSet<Base> Base { get; set; }
         public DbSet<PoundToKilo> PoundsToKilos { get; set; }
-        public DBSet<HorsetoBlock> HorsestoBlocks { get; set; }
+        public DbSet<HorsetoBlock> HorsestoBlocks { get; set; }
     }
 }
